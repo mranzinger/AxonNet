4fe906d385ad0d2cc7d69f87589768beba210391
Mike Ranzinger <mikeranzinger@gmail.com>
2014-05-20 21:51:56 -0600

    Working on updating the cost objects to the new interface

---
+++ include/binary_cost.h
@@ -0,0 +1,13 @@
+/*
+ * binary_cost.h
+ *
+ *  Created on: May 20, 2014
+ *      Author: mike
+ */
+
+
+#pragma once
+
+#include "i_cost.h"
+
+
---
+++ src/binary_cost.cpp
@@ -0,0 +1,10 @@
+/*
+ * binary_cost.cpp
+ *
+ *  Created on: May 20, 2014
+ *      Author: mike
+ */
+
+#include "binary_cost.h"
+
+
