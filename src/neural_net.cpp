a3dd7927577f4b78e5c36792ac22cf5ba8cfab9a
mranzinger <mikeranzinger@gmail.com>
2014-05-21 10:10:30 -0600

    Fixed the cost computation for the Test function

--- src/neural_net.cpp
+++ src/neural_net.cpp
@@ -356,7 +356,7 @@ void NeuralNet::Test(ITrainProvider &provider, const std::string &chkRoot)
 
 		Real err = _cost->Compute(op, labels);
 
-		testErr += err;
+		testErr += err * batchSize;
 
 		MaxBinarize(op.Data);
 
