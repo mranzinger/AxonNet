5f70750d2fd988b23b1169c8eb6cc469de35006d
Mike Ranzinger <mikeranzinger@gmail.com>
2014-05-24 01:46:28 -0600

    Finished the initial implementation of the mrf layer

--- src/mrf_layer.cpp
+++ src/mrf_layer.cpp
@@ -7,11 +7,13 @@
 
 #include "mrf_layer.h"
 
+#include "neural_net.h"
+
 using namespace std;
 
 MRFLayer::MRFLayer(std::string name,
 				   size_t width, size_t height)
-	: SingleInputLayer(move(name)),
+	: LayerBase(move(name)),
 	  _width(width), _height(height)
 {
 }
@@ -32,11 +34,14 @@ void MRFLayer::Compute(ParamMap& inputMap, bool isTraining)
 
 	for (int imageIdx = 0; imageIdx < input.BatchSize(); ++imageIdx)
 	{
-		CMap outputField(output.Data.data() + imageIdx * output.size(),
+		RMap outputField(output.Data.data() + imageIdx * output.size(),
 						 _height, _width * input.Depth);
 
-		RMap inputField(const_cast<Real*>(input.Data.data()) + imageIdx * input.size(),
+		const RMap inputField(const_cast<Real*>(input.Data.data()) + imageIdx * input.size(),
 						input.Height, input.Width * input.Depth);
+
+		UMapVector vecCoords(coordsOut.Data.data() + imageIdx * 2 * input.Depth,
+							 2 * input.Depth);
 
 		// A summed area table is the most efficient way to compute the
 		// windowed sum over a scanned region. The only weird thing about
@@ -84,14 +89,80 @@ void MRFLayer::Compute(ParamMap& inputMap, bool isTraining)
 			{
 				for (int layer = 0; layer < input.Depth; ++layer)
 				{
+					const pair<int, int> &basePt = coords[layer];
 
+					const int srcRow = basePt.first + row;
+					const int srcCol = (basePt.second + col) * input.Depth + layer;
+
+					const Real val = inputField(srcRow, srcCol);
+
+					const int destCol = col * input.Depth + layer;
+
+					outputField(row, destCol) = val;
 				}
 			}
+		}
+
+		// Write out the coordinates
+		for (int i = 0; i < coords.size(); ++i)
+		{
+			const pair<int,int> &coord = coords[i];
+			vecCoords(i * 2) = coord.first;
+			vecCoords(i * 2 + 1) = coord.second;
 		}
 	}
 
 	inputMap[_name] = move(output);
 	inputMap[_name + "-coords"] = move(coordsOut);
+}
+
+void MRFLayer::Backprop(const ParamMap& computeMap, ParamMap& inputErrorMap)
+{
+	const string &inputName = GetInputName();
+
+	const Params &lastInput = *GetData(computeMap, inputName);
+	const Params &outputErrs = *GetData(inputErrorMap, _name);
+	const Params &outputCoords = *GetData(computeMap, _name + "-coords");
+
+	Params inputErrors(lastInput,
+			  CMatrix::Zero(lastInput.size(), lastInput.BatchSize()));
+
+	for (int imageIdx = 0; imageIdx < lastInput.BatchSize(); ++imageIdx)
+	{
+		const RMap imgOutputErrs(const_cast<Real*>(outputErrs.Data.data()) + imageIdx * outputErrs.size(),
+						   outputErrs.Height, outputErrs.Width * outputErrs.Depth);
+		const UMapVector imgOutputCoords(const_cast<Real*>(outputCoords.Data.data()) + imageIdx * outputCoords.size(),
+						   outputCoords.size());
+
+		RMap imgInputErrs(inputErrors.Data.data() + imageIdx * inputErrors.size(),
+						  inputErrors.Height, inputErrors.Width * inputErrors.Depth);
+
+		vector<pair<int,int>> iCoords(lastInput.Depth);
+		for (int i = 0; i < imgOutputCoords.size(); i += 2)
+			iCoords.emplace_back(imgOutputCoords(i), imgOutputCoords(i + 1));
+
+		for (int row = 0; row < imgOutputErrs.rows(); ++row)
+		{
+			for (int col = 0; col < imgOutputErrs.cols(); ++col)
+			{
+				for (int layer = 0; layer < lastInput.Depth; ++layer)
+				{
+					const pair<int, int> &baseCoord = iCoords[layer];
+
+					const int opCol = col * lastInput.Depth + layer;
+
+					const Real opVal = imgOutputErrs(row, opCol);
+
+					const int ipRow = baseCoord.first + row;
+					const int ipCol = (baseCoord.second + col) * lastInput.Depth + layer;
+
+					imgInputErrs(ipRow, ipCol) = opVal;
+				}
+			}
+		}
+	}
+
+	inputErrorMap[inputName] = move(inputErrors);
 }
 
 void MRFLayer::CalcSAT(const RMap& inputField, RMatrix& sumAreaTable, int depth) const
@@ -116,11 +187,6 @@ void MRFLayer::CalcSAT(const RMap& inputField, RMatrix& sumAreaTable, int depth)
 	}
 }
 
-void MRFLayer::Backprop(const ParamMap& computeMap, ParamMap& inputErrorMap)
-{
-	throw runtime_error("Not implemented yet.");
-}
-
 const std::string& MRFLayer::GetInputName()
 {
 	if (!_inputName.empty())
@@ -140,7 +206,7 @@ const std::string& MRFLayer::GetInputName()
 
 void BindStruct(const aser::CStructBinder &binder, MRFLayer &layer)
 {
-	BindStruct(binder, (SingleInputLayer&)layer);
+	BindStruct(binder, (LayerBase&)layer);
 
 	binder("width", layer._width)
 		  ("height", layer._height);
